1860574cbeb860a8ab2bd4593ebf34b309a8f16c
DOUCKS <doucksadam@gmail.com>
2023-02-08 18:56:40 +0100

    page acceuil

--- index.php
+++ index.php
@@ -1,98 +1,265 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/override-bootstrap.css">
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    
-    <title>Cuisinea - Senegalaise </title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/override-bootstrap.css">
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+  <title>Cuisinea - Senegalaise </title>
 </head>
+
 <body>
-<div class="container">
+  <div class="container">
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32" role="img" aria-label="Amorcer"><use xlink:href="#bootstrap"></use></svg>
+      <a href="index.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32" role="img" aria-label="Amorcer">
+          <use xlink:href="#bootstrap"></use>
+        </svg>
+        <img src="assets/images/logo-cuisinea-horizontal.jpg" width="300" alt="cuisinea">
       </a>
 
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" class="nav-link px-2 link-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Maison</font></font></a></li>
-        <li><a href="#" class="nav-link px-2 link-dark"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Caractéristiques</font></font></a></li>
-        <li><a href="#" class="nav-link px-2 link-dark"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Tarification</font></font></a></li>
-        <li><a href="#" class="nav-link px-2 link-dark"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">FAQ</font></font></a></li>
-        <li><a href="#" class="nav-link px-2 link-dark"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">À propos</font></font></a></li>
+        <li><a href="#" class="nav-link px-2 link-secondary">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">Acceuil</font>
+            </font>
+          </a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">Nos recettes</font>
+            </font>
+          </a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">Nos tarifs</font>
+            </font>
+          </a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">FAQ</font>
+            </font>
+          </a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">À propos</font>
+            </font>
+          </a></li>
       </ul>
 
       <div class="col-md-3 text-end">
-        <button type="button" class="btn btn-outline-primary me-2"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Connexion</font></font></button>
-        <button type="button" class="btn btn-primary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">S'inscrire</font></font></button>
+        <button type="button" class="btn btn-outline-primary me-2">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Connexion</font>
+          </font>
+        </button>
+        <button type="button" class="btn btn-primary">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">S'inscrire</font>
+          </font>
+        </button>
       </div>
     </header>
   </div>
 
   <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-      <div class="col-10 col-sm-8 col-lg-6">
-        <img src="bootstrap-themes.png" class="d-block mx-lg-auto img-fluid" alt="Thèmes d'amorçage" width="700" height="500" loading="lazy">
+    <div class="col-10 col-sm-8 col-lg-6">
+      <img src="assets/images/logo-cuisinea.jpg" class="d-block mx-lg-auto img-fluid" alt="Thèmes d'amorçage" width="350" loading="lazy">
+    </div>
+    <div class="col-lg-6">
+      <h1 class="display-5 fw-bold lh-1 mb-3">Cuisinea - Senegalaise</h1>
+      <p class="lead">
+        <font style="vertical-align: inherit;">
+          <font style="vertical-align: inherit;"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ab consectetur consequatur dignissimos officia voluptatem, repellat quia tempora sequi enim facilis delectus porro nemo beatae deleniti obcaecati unde, dicta assumenda. </font>
+        </font>
+      </p>
+      <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+        <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">voir nos recettes</font>
+          </font>
+        </button>
       </div>
-      <div class="col-lg-6">
-        <h1 class="display-5 fw-bold lh-1 mb-3"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Héros réactif aligné à gauche avec image</font></font></h1>
-        <p class="lead"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Concevez et personnalisez rapidement des sites mobiles réactifs avec Bootstrap, la boîte à outils open source frontale la plus populaire au monde, comprenant des variables et des mixins Sass, un système de grille réactif, de nombreux composants prédéfinis et de puissants plug-ins JavaScript.</font></font></p>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Primaire</font></font></button>
-        </div>
-      </div>
-
-      <div class="container">
-  <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
-    <div class="col mb-3">
-      <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-      </a>
-      <p class="text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">© 2022</font></font></p>
     </div>
 
-    <div class="col mb-3">
 
-    </div>
+<!--°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°Les cartes°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°--->
 
-    <div class="col mb-3">
-      <h5><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Section</font></font></h5>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Maison</font></font></a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Caractéristiques</font></font></a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Tarification</font></font></a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">FAQ</font></font></a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">À propos</font></font></a></li>
-      </ul>
-    </div>
-
-    <div class="col mb-3">
-      <h5><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Section</font></font></h5>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Maison</font></font></a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Caractéristiques</font></font></a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Tarification</font></font></a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">FAQ</font></font></a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">À propos</font></font></a></li>
-      </ul>
-    </div>
-
-    <div class="col mb-3">
-      <h5><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Section</font></font></h5>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Maison</font></font></a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Caractéristiques</font></font></a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Tarification</font></font></a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">FAQ</font></font></a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">À propos</font></font></a></li>
-      </ul>
-    </div>
-  </footer>
+<div class="row">
+  <div class="col-md-4">
+  <div class="card">
+  <img src="uploads/recipes/1-chocolate-au-mousse.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h<5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
 </div>
+  </div>
+
+  <div class="col-md-4">
+  <div class="card">
+  <img src="uploads/recipes/2-gratin-dauphinois.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h<5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+
+  <div class="col-md-4">
+  <div class="card">
+  <img src="uploads/recipes/3-salade.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h<5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+
+</div>
+
+
+
+
+
+
+
+<!--°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°Footer°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°--->
+
     </div>
+    <div class="container">
+      <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+        <div class="col mb-3">
+          <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+            <svg class="bi me-2" width="40" height="32">
+              <use xlink:href="#bootstrap"></use>
+            </svg>
+          </a>
+          <p class="text-muted">
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">© 2022</font>
+            </font>
+          </p>
+        </div>
+
+        <div class="col mb-3">
+
+        </div>
+
+        <div class="col mb-3">
+          <h5>
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">Section</font>
+            </font>
+          </h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">
+                <font style="vertical-align: inherit;">
+                  <font style="vertical-align: inherit;">Maison</font>
+                </font>
+              </a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">
+                <font style="vertical-align: inherit;">
+                  <font style="vertical-align: inherit;">Caractéristiques</font>
+                </font>
+              </a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">
+                <font style="vertical-align: inherit;">
+                  <font style="vertical-align: inherit;">Tarification</font>
+                </font>
+              </a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">
+                <font style="vertical-align: inherit;">
+                  <font style="vertical-align: inherit;">FAQ</font>
+                </font>
+              </a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">
+                <font style="vertical-align: inherit;">
+                  <font style="vertical-align: inherit;">À propos</font>
+                </font>
+              </a></li>
+          </ul>
+        </div>
+
+        <div class="col mb-3">
+          <h5>
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">Section</font>
+            </font>
+          </h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">
+                <font style="vertical-align: inherit;">
+                  <font style="vertical-align: inherit;">Maison</font>
+                </font>
+              </a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">
+                <font style="vertical-align: inherit;">
+                  <font style="vertical-align: inherit;">Caractéristiques</font>
+                </font>
+              </a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">
+                <font style="vertical-align: inherit;">
+                  <font style="vertical-align: inherit;">Tarification</font>
+                </font>
+              </a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">
+                <font style="vertical-align: inherit;">
+                  <font style="vertical-align: inherit;">FAQ</font>
+                </font>
+              </a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">
+                <font style="vertical-align: inherit;">
+                  <font style="vertical-align: inherit;">À propos</font>
+                </font>
+              </a></li>
+          </ul>
+        </div>
+
+        <div class="col mb-3">
+          <h5>
+            <font style="vertical-align: inherit;">
+              <font style="vertical-align: inherit;">Section</font>
+            </font>
+          </h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">
+                <font style="vertical-align: inherit;">
+                  <font style="vertical-align: inherit;">Maison</font>
+                </font>
+              </a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">
+                <font style="vertical-align: inherit;">
+                  <font style="vertical-align: inherit;">Caractéristiques
+              </a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">
+                <font style="vertical-align: inherit;">
+                  <font style="vertical-align: inherit;">Tarifications</font>
+                </font>
+              </a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">
+                <font style="vertical-align: inherit;">
+                  <font style="vertical-align: inherit;">FAQ</font>
+                </font>
+              </a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">
+                <font style="vertical-align: inherit;">
+                  <font style="vertical-align: inherit;">À propos</font>
+                </font>
+              </a></li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+  </div>
 
 </body>
+
 </html>
